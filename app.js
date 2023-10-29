867eed96638bf563fe49c531ed62a15e7d81711a
chutrinh <125455926+chutrinh@users.noreply.github.com>
2023-10-29 16:48:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -38,30 +38,30 @@ app.use(
 );
 
 // nhận file ảnh
-// const multer = require("multer");
-// const fileStore = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + "-" + file.originalname);
-//   },
-// });
-// const fileFileter = (req, file, cb) => {
-//   if (
-//     file.mimetype === "image/jpeg" ||
-//     file.mimetype === "image/png" ||
-//     file.mimetype === "image/jpeg"
-//   ) {
-//     cb(null, true);
-//   } else {
-//     cb(null, false);
-//   }
-// };
-// app.use(
-//   multer({ storage: fileStore, fileFilter: fileFileter }).array("images")
-// );
-// app.use("/images", express.static(path.join(__dirname, "images")));
+const multer = require("multer");
+const fileStore = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+const fileFileter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+app.use(
+  multer({ storage: fileStore, fileFilter: fileFileter }).array("images")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 const bodyParser = require("body-parser");
 // const cookieParser = require("cookie-parser");
@@ -70,17 +70,17 @@ app.use(bodyParser.json());
 // app.use(cookieParser());
 
 const authRouter = require("./router/auth-router");
-// const homeRouter = require("./router/home-router");
-// const adminRouter = require("./router/admin-router");
-// const chatRouter = require("./router/chat-router");
+const homeRouter = require("./router/home-router");
+const adminRouter = require("./router/admin-router");
+const chatRouter = require("./router/chat-router");
 app.use("/auth", authRouter);
-// app.use("/admin", adminRouter);
-// app.use("/chat", chatRouter);
-// app.use(homeRouter);
+app.use("/admin", adminRouter);
+app.use("/chat", chatRouter);
+app.use(homeRouter);
 
 // handing err
-// const { handingErrors } = require("./middleware/handing-errors");
-// app.use(handingErrors);
+const { handingErrors } = require("./middleware/handing-errors");
+app.use(handingErrors);
 
 mongoose
   .connect(
@@ -88,55 +88,55 @@ mongoose
   )
   .then(() => {
     const server = app.listen(process.env.PORT || 5000);
-    // const io = require("socket.io")(server, {
-    //   cors: {
-    //     origin: ["http://localhost:3000", "http://localhost:3001"],
-    //     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
-    //     credentials: true,
-    //   },
-    // });
+    const io = require("socket.io")(server, {
+      cors: {
+        origin: ["http://localhost:3000", "http://localhost:3001"],
+        methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
+        credentials: true,
+      },
+    });
 
-    // io.on("connection", (socket) => {
-    //   // thông báo kết nối đến client
-    //   console.log("connected to client");
+    io.on("connection", (socket) => {
+      // thông báo kết nối đến client
+      console.log("connected to client");
 
-    //   // lắng nghe sự kiện submit tin nhắn của người dùng lên server
-    //   socket.on("chat message", (msg) => {
-    //     let message;
-    //     if ("msgClient" in msg) {
-    //       message = { msgClient: msg.msgClient };
-    //     }
-    //     if ("msgAdmin" in msg) {
-    //       message = { msgAdmin: msg.msgAdmin };
-    //     }
+      // lắng nghe sự kiện submit tin nhắn của người dùng lên server
+      socket.on("chat message", (msg) => {
+        let message;
+        if ("msgClient" in msg) {
+          message = { msgClient: msg.msgClient };
+        }
+        if ("msgAdmin" in msg) {
+          message = { msgAdmin: msg.msgAdmin };
+        }
 
-    //     Chat.findOne({ roomId: msg.roomId })
-    //       .then((room) => {
-    //         if (room) {
-    //           room.content.push(message);
-    //           return room.save();
-    //         } else {
-    //           const newRoom = new Chat({
-    //             roomId: msg.roomId,
-    //             content: message,
-    //           });
-    //           return newRoom.save();
-    //         }
-    //       })
-    //       .then((room) => {
-    //         // gửi tin nhắn cho client
-    //         Chat.find().then((chats) => {
-    //           console.log(chats);
-    //           socket.broadcast.emit("chat message", chats);
-    //           socket.emit("chat message", chats);
-    //         });
-    //       });
-    //   });
+        Chat.findOne({ roomId: msg.roomId })
+          .then((room) => {
+            if (room) {
+              room.content.push(message);
+              return room.save();
+            } else {
+              const newRoom = new Chat({
+                roomId: msg.roomId,
+                content: message,
+              });
+              return newRoom.save();
+            }
+          })
+          .then((room) => {
+            // gửi tin nhắn cho client
+            Chat.find().then((chats) => {
+              console.log(chats);
+              socket.broadcast.emit("chat message", chats);
+              socket.emit("chat message", chats);
+            });
+          });
+      });
 
-    //   // kiểm tra kết nối
-    //   socket.on("disconnect", () => {
-    //     console.log("người dùng thoát");
-    //   });
-    // });
+      // kiểm tra kết nối
+      socket.on("disconnect", () => {
+        console.log("người dùng thoát");
+      });
+    });
     console.log("connect successfuly!");
   });
